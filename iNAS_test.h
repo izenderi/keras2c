963412de5080cac75c3da5bfc3cad6c931c75d10
izenderi <ziliang1121@gmail.com>
2023-02-13 12:42:00 -0800

    r10-1.0.1: generate c files

---
+++ iNAS_test.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void iNAS_test(k2c_tensor* INPUT_input, k2c_tensor* FC_END_output); 
+void iNAS_test_initialize(); 
+void iNAS_test_terminate(); 

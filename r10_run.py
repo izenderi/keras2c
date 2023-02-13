46189c34201a6e8e9806939db5e44d68ee985b1e
izenderi <ziliang1121@gmail.com>
2023-02-13 15:02:51 -0800

    added csv runtime load malloc

--- r10_run.py
+++ r10_run.py
@@ -1,2 +1,2 @@
 from keras2c import k2c
-k2c("/home/johnson/Developer/Git/iNAS/iNAS/weights/cifar_example_run/temp_network_cifar_example_run.h5", "iNAS_test", malloc=False, num_tests=10, verbose=True)
+k2c("/home/johnson/Developer/Git/iNAS/iNAS/weights/cifar_example_run/temp_network_cifar_example_run.h5", "iNAS_csv", malloc=True, num_tests=10, verbose=True)
